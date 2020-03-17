7d259d4204d6df4c080d3d99946538770303c4eb
asengelhart <asengelhart@yahoo.com>
2020-03-16 20:52:13 -0600

    user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(username: "Jim", email: "jim@example.com", password: "password")
+    @invalid_user1 = User.new(email: "jim@example.com", password: "password")
+    @invalid_user2 = User.new(username: "Jim", password: "password")
+    @invalid_user3 = User.new(username: "Jim", email: "jim@example.com")
+  end
 
   it "has username, password and email" do
-    user = User.new(username: "Jim", email: "jim@example.com", password: "password")
-    expect(user).to be_valid
+    expect(@user).to be_valid
+  end
+
+  it "is invalid without a username, password, or email" do
+
+    expect(@invalid_user1).not_to be_valid
+    expect(@invalid_user2).not_to be_valid
+    expect(@invalid_user3).not_to be_valid
   end
 end
