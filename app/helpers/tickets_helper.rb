f0e7606ce585014378fe9b25faa0451a287b475b
asengelhart <asengelhart@yahoo.com>
2020-03-22 16:16:08 -0600

    work on selecting user for ticket

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,9 +1,9 @@
 module TicketsHelper
-  def user_field(user_id, display_user_list)
+  def user_field(form, user_id, display_user_list)
     if display_user_list
-      f.collection_select :user_id, User.all, :id, :email
+      form.collection_select :user_id, User.all, :id, :email
     else
-      f.hidden_field :user_id, user_id
+      form.hidden_field :user_id, user_id
     end
   end
 end
